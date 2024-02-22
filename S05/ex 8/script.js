5eb6f244b5355b802403f33146faef5b9370b2c9
Frank Osorio <frankosorio4@gmail.com>
2024-02-22 20:38:18 -0300

    modifying ex 8 in the branch

--- S05/ex 8/script.js
+++ S05/ex 8/script.js
@@ -2,12 +2,21 @@
 
 
 let nome = document.getElementById('liNome');
-let nome = document.getElementById('liIdade');
-let nome = document.getElementById('liNacionalidade');
-let nome = document.getElementById('liProfissao');
+let inputNome = document.getElementById('inputNome');
+let idade = document.getElementById('liIdade');
+let inputIdade = document.getElementById('inputIdade');
+let nacionalidade = document.getElementById('liNacionalidade');
+let profissao = document.getElementById('liProfissao');
 
+function mudarNome(){
+    nome.innerText = 'Nome: ' + inputNome.value;
+    nome.style.background = 'lightgreen'
+}
 
-
+function mudarIdade(){
+    idade.innerText = 'Idade: ' + inputIdade.value + ' anos';
+    idade.style.background = 'lightgreen'
+}
 
 // function boasVindas(){
 //     alert("Seja bem-vindo ao meu site.")
