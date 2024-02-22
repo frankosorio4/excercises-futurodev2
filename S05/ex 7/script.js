768fdfa67661c0f5fe797beb1912df72f9b78389
Frank Osorio <frankosorio4@gmail.com>
2024-02-22 18:15:32 -0300

    adding ex 7

---
+++ S05/ex 7/script.js
@@ -0,0 +1,5 @@
+// Crie um arquivo JavaScript chamado script.js e adicione uma função simples que escreva uma mensagem de boas-vindas no alert.
+
+function boasVindas(){
+    alert("Seja bem-vindo ao meu site.")
+}
