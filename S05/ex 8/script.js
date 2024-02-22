8fd7aa248f95852c904005f2475a7550a88c10b4
Frank Osorio <frankosorio4@gmail.com>
2024-02-22 19:29:27 -0300

    ex 8

---
+++ S05/ex 8/script.js
@@ -0,0 +1,14 @@
+// No arquivo JavaScript, selecione elementos HTML (por exemplo, o elemento que contém o nome do usuário) e atualize seu conteúdo dinamicamente usando o JavaScript.
+
+
+let nome = document.getElementById('liNome');
+let nome = document.getElementById('liIdade');
+let nome = document.getElementById('liNacionalidade');
+let nome = document.getElementById('liProfissao');
+
+
+
+
+// function boasVindas(){
+//     alert("Seja bem-vindo ao meu site.")
+// }
