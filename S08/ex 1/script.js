65f6034227e25eb635579094174e9cacd9229d81
Frank Osorio <frankosorio4@gmail.com>
2024-03-12 21:15:55 -0300

    Changes

--- S08/ex 1/script.js
+++ S08/ex 1/script.js
@@ -5,5 +5,5 @@ let idSelected = document.querySelector('#idSelected');
 function changeBckg(){
     // console.log(tagSelected );
     idSelected.style.backgroundColor= 'red';
-    idSelected.style.width = '250px';
+    idSelected.style.width = '180px';
 }
