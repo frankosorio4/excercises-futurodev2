ad00aa19ee0f3f2b5fe97b5e84dc50b8f2c75f76
Frank Osorio <frankosorio4@gmail.com>
2024-03-14 15:23:32 -0300

    adding ex 6

---
+++ S08 dom JS/ex 6 adding class/script.js
@@ -0,0 +1,15 @@
+console.log("script.js");
+
+let list2 = document.getElementById('list2');
+console.log(list2);
+
+function modifyStyle(){
+    list2.setAttribute('class','list');
+    //console.log(list2);
+    let l = list2.children.length;
+    //console.log(l);
+    for (let i = 0; i < l; i++){
+        list2.children[i].setAttribute('class','elementLi');
+    }
+    console.log(list2);
+}
