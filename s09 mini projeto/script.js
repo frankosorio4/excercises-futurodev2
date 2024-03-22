944a6fd977e0e16caf355afcba84764c00ea9145
Frank Osorio <frankosorio4@gmail.com>
2024-03-22 18:15:21 -0300

    adding timer

--- s09 mini projeto/script.js
+++ s09 mini projeto/script.js
@@ -3,36 +3,37 @@ console.log('script.js')
 let divMinutos = document.getElementById('divMinutos');
 let divSegundos = document.getElementById('divSegundos');
 let divMs = document.getElementById('divMs');
+let iniciarBtn = document.getElementById('iniciarBtn');
 
 let interval;
 let ms = 1000;
-let segundos = 10;
-let minutos = 12;//25 minutos
-let flag1 = 0;
-// let flag2 = 0;
+let segundos = 10;// cambiar a 60
+let minutos = 12;// Inicial time
+let iniciarInterval = true;// To allow start only start once the interval and avoid to start again without pause first
+
 
 function startTimer() {
-    if(flag1 === 0){
-        flag1 = 1;
+    if(iniciarInterval){
+        iniciarInterval = false;
         interval = setInterval(updateTimer, 10)
     }
 }
 
 function updateTimer(){
     ms -= 10;
-    console.log(ms);
+    //console.log(ms);
     divMs.innerHTML = formatMs(ms);
-    if (segundos === 10){
-        minutos--;
-        segundos--;
-        divSegundos.innerHTML = formatTime(segundos);
-        divMinutos.innerHTML = formatTime(minutos);
-    }
     if (ms === 0){
         ms = 1000;
         segundos--;
         // console.log(segundos);
         divSegundos.innerHTML = formatTime(segundos);
+    }
+    if (segundos === 10){
+        minutos--;
+        segundos--;
+        divSegundos.innerHTML = formatTime(segundos);
+        divMinutos.innerHTML = formatTime(minutos);
     }
     if (segundos === -1){
         segundos = 10;
@@ -41,8 +42,23 @@ function updateTimer(){
 }
 
 function pauseTimer(){
+    if (!iniciarInterval){
+        clearInterval(interval);
+        iniciarInterval = true;
+        iniciarBtn.innerHTML = 'Continuar';
+    }
+}
+
+function resetTimer(){
     clearInterval(interval);
-    flag1 = 0;
+    iniciarInterval = true;
+    iniciarBtn.innerHTML = 'Iniciar';
+    divMinutos.innerHTML = '12';
+    divSegundos.innerHTML = '00';
+    divMs.innerHTML = '000';
+    ms = 1000;
+    segundos = 10;
+    minutos = 12;
 }
 
 function formatTime(time) {
